eb0860a20258decdc18505d6dfb6003d5fb2b10b
spankoj <spanyesz@gmail.com>
2021-06-29 15:54:45 +0200

    Add add-page with form input fields

--- pages/add-data.js
+++ pages/add-data.js
@@ -1,29 +1,42 @@
 import Link from 'next/link';
 import React from 'react';
 import Layout from '../components/Layout';
+import styles from '../styles/AddData.module.css';
 
 function addData() {
   return (
     <Layout title="Add Data">
       <div>
-        <form>
-          <div>
-            <label>Image name</label>
-            <input />
+        <form className={styles.form}>
+          <div className={styles.control}>
+            <label className={styles.label} htmlFor="name">
+              Descriptive Name
+            </label>
+            <input
+              className={styles.input}
+              type="text"
+              name="name"
+              placeholder="best-drone-image-ever"
+            />
+          </div>
+          <div className={styles.control}>
+            <label className={styles.label}>Category</label>
+            <select className={styles.input} name="category" id="id">
+              <option>Building</option>
+              <option>Monument</option>
+              <option>Landscape</option>
+            </select>
           </div>
           <div>
-            <label>Category</label>
-            <input />
+            <label className={styles.label}>Upload image</label>
+            <input className={styles.input} type="file" />
           </div>
-          <div>
-            <label>Upload image</label>
-            <input type="file" />
-          </div>
+          <input className="btn" type="submit" />
         </form>
       </div>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
+      {/* <Link href="/">
+        <a className="btn-secondary">Home</a>
+      </Link> */}
     </Layout>
   );
 }
