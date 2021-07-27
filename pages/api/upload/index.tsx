07c797b672aa5343269843f8d371369db685596f
spankoj <spanyesz@gmail.com>
2021-07-27 11:49:35 +0200

    cloudinary API keys and API endpoint taken out

--- pages/api/upload/index.tsx
+++ pages/api/upload/index.tsx
@@ -1,17 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { v2 as c } from 'cloudinary';
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next';
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// import { v2 as c } from 'cloudinary';
+// // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import type { NextApiRequest, NextApiResponse } from 'next';
 
-const endpoint = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const options = {
-      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    };
-    c.config(options);
-  }
-};
+// const endpoint = async (req: NextApiRequest, res: NextApiResponse) => {
+//   if (req.method === 'POST') {
+//     const options = {
+//       cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME,
+//       api_key: process.env.CLOUDINARY_API_KEY,
+//       api_secret: process.env.CLOUDINARY_API_SECRET,
+//     };
+//     c.config(options);
+//   }
+// };
 
-export default endpoint;
+// export default endpoint;
